95809bb164a9b78d3b08afd96bafb90cb7967448
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 00:03:54 +0700

    membuat objek controller dan menjalankan controller

--- public/index.php
+++ public/index.php
@@ -2,9 +2,15 @@
 require_once __DIR__."/../vendor/autoload.php";
 
 use Program\PHPMVC\App\Router;
+use Program\PHPMVC\Controller\HomeController;
 
-Router::add("GET","/","HomeController","index");
-Router::add("GET","/login","UserController","login");
-Router::add("GET","/register","UserController","register");
+// Router::add("GET","/","HomeController","index");
+// Router::add("GET","/login","UserController","login");
+// Router::add("GET","/register","UserController","register");
+
+
+Router::add("GET","/", HomeController::class,"index");
+Router::add("GET","/hello", HomeController::class,"hello");
+Router::add("GET","/world", HomeController::class,"world");
 
 Router::run();
