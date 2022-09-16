0a46b9284fb08752817779a3bd8886eb50a0512e
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 11:32:33 +0700

    Implelementasi product controller dan path variable ke route

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 require_once __DIR__."/../vendor/autoload.php";
 
 use Program\PHPMVC\App\Router;
-use Program\PHPMVC\Controller\HomeController;
+use Program\PHPMVC\Controller\{HomeController,ProductController};
 
 // Router::add("GET","/","HomeController","index");
 // Router::add("GET","/login","UserController","login");
 // Router::add("GET","/register","UserController","register");
 
+Router::add("GET","/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)",ProductController::class,"categories");
 
 Router::add("GET","/", HomeController::class,"index");
 Router::add("GET","/hello", HomeController::class,"hello");
