427444299191c51791257fa9b3efc6700392ed34
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-16 11:57:05 +0700

    Implementasi class view ke dalam HomeController:

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,9 +1,12 @@
 <?php
 namespace Program\PHPMVC\Controller;
+use Program\PHPMVC\App\View;
+
 
 class HomeController{
     public function index():void{
         $model=["title" => "Home Page", "body" => "Welcome to my web"];
+        View::render("Home/index", $model);
     }
     
     public function hello():void{
@@ -18,7 +21,7 @@ class HomeController{
         $request=["username" => $_POST["username"], 
                  "password" => $_POST["password"]
                 ];
-
+        
         $response=[
             "message" => "Login Sukses"
         ];
