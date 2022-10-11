afc34a6e7f13b835ec4cdd73c678f1fecb00f1d7
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-11 22:50:42 +0700

    Menambahkan components header dan footer

--- app/App/View.php
+++ app/App/View.php
@@ -3,7 +3,9 @@ namespace Program\PHPMVC\App;
 
 class View{
     public static function render(string $view, $model=""){
+        require __DIR__."/../View/components/header.php";
         require __DIR__."/../View/".$view.".php";
+        require __DIR__."/../View/components/footer.php";
     }
 }
 ?>
