4b1e09183f547a149aad7650901cdd8146831c27
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-13 13:34:45 +0700

    Membuat route untuk login dan register

--- public/index.php
+++ public/index.php
@@ -2,9 +2,18 @@
 require_once __DIR__."/../vendor/autoload.php";
 
 use Program\PHPMVC\App\Router;
-use Program\PHPMVC\Controller\{HomeController};
+use Program\PHPMVC\Config\Database;
+use Program\PHPMVC\Controller\{HomeController, UserController};
 
+Database::getConnection('production');
 
+// Home Controller
 Router::add("GET","/", HomeController::class,"index");
+
+// User Controller
+Router::add("GET","/users/register", UserController::class,"register");
+Router::add("POST","/users/register", UserController::class,"postRegister");
+Router::add("GET","/users/login", UserController::class,"login");
+Router::add("POST","/users/login", UserController::class,"postLogin");
 
 Router::run();
