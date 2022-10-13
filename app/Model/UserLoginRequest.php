7eb5c6523568b353af8da9a6b2d450782e9980f6
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-13 13:32:34 +0700

    Membuat model user login request dan response

---
+++ app/Model/UserLoginRequest.php
@@ -0,0 +1,8 @@
+<?php
+namespace Program\PHPMVC\Model;
+
+class UserLoginRequest{
+    public ?string $name=null;
+    public ?string $password=null;
+}
+?>
---
+++ app/Model/UserLoginResponse.php
@@ -0,0 +1,9 @@
+<?php
+namespace Program\PHPMVC\Model;
+
+use Program\PHPMVC\Domain\User;
+
+class UserLoginResponse{
+  public User $user;
+}
+?>
