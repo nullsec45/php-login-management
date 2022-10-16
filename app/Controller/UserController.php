8649582415fe7bf3adcfb371950587c22b525ab1
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-16 15:49:56 +0700

    Menambahkan session di method postLogin

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -7,17 +7,23 @@ use Program\PHPMVC\Config\Database;
 use Program\PHPMVC\Exception\ValidationException;
 use Program\PHPMVC\Model\UserLoginRequest;
 use Program\PHPMVC\Model\UserRegisterRequest;
+use Program\PHPMVC\Repository\SessionRepository;
 use Program\PHPMVC\Repository\UserRepository;
+use Program\PHPMVC\Service\SessionService;
 use Program\PHPMVC\Service\UserService;
 
 class UserController{
     private UserService $userService;
+    private SessionService $sessionService;
     
     public function __construct()
     {
         $conection=Database::getConnection();
         $userRepository=new UserRepository($conection);
         $this->userService=new UserService($userRepository);
+
+        $sessionRepository=new SessionRepository($conection);
+        $this->sessionService=new SessionService($sessionRepository, $userRepository);
     }
     public function register(){
         View::render("User/register", ["title" => "Register new User"]);
@@ -45,7 +51,8 @@ class UserController{
         $request->password=$_POST["password"];
 
         try{
-            $this->userService->login($request);
+            $response=$this->userService->login($request);
+            $this->sessionService->create($response->user->id);
             View::redirect("/");
         }catch(ValidationException $exception){
             View::render("User/login", ["title" => "Login User","error" => $exception->getMessage()]);
