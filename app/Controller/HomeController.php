448397526353b2a13a960bcd25fda7c19358c848
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-16 15:50:39 +0700

    validasi session di Home Controller

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,10 +1,31 @@
 <?php
 namespace Program\PHPMVC\Controller;
 use Program\PHPMVC\App\View;
-
+use Program\PHPMVC\Config\Database;
+use Program\PHPMVC\Repository\SessionRepository;
+use Program\PHPMVC\Repository\UserRepository;
+use Program\PHPMVC\Service\SessionService;
 
 class HomeController{
+   private SessionService $sessionService;
+
+   public function __construct()
+   {
+      $connection=Database::getConnection();
+      $sessionRepository=new SessionRepository($connection);
+      $userRepository=new UserRepository($connection);
+      $this->sessionService=new SessionService($sessionRepository, $userRepository);
+   }
    public function index(){
-        View::render("Home/index",["title" => "PHP Login Management"]);
+        $user=$this->sessionService->current();
+
+        if($user == null){
+          View::render("Home/index",["title" => "PHP Login Management"]);
+        }else{
+          View::render("Home/dashboard",["title" => "Dashboard","user" => [
+            "name" => $user->name
+          ]]);
+        }
+         
    }
 }
